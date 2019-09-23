09c57acd7233067a354a44cd7c5006ca94229ccc
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-23 17:32:19 -0400

    2.10

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -19,7 +19,7 @@ console.log(job);
 
 /*******************************
  * Variable mutation and type coercion
- */
+ 
 
  let firstName = 'John';
  let age = 28;
@@ -41,3 +41,32 @@ console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? 
 
 let lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+*/
+
+/***********************************
+ * Basic Operators
+ */
+let year, yearJohn, yearMark;
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+let johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+let x;
+console.log(typeof x);
