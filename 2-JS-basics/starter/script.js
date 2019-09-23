b7684761463bae804f8179cf90958144ef808523
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-23 16:31:02 -0400

    section 2.8: log variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World!!!')
+let firstName = 'John';
+console.log(firstName);
+
+let lastName = 'Smith';
+let age = 28;
+
+let fullAge = true;
+console.log(fullAge);
+
+let job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
