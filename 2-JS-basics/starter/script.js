bcaa3ddef235a2a53f59d4f35880ea441ec3014a
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-23 16:43:36 -0400

    2.9: type coercion and variable manipulation

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,6 @@
+/****************************
+ * Variables and Data Types
+ 
 let firstName = 'John';
 console.log(firstName);
 
@@ -12,3 +15,29 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+*/
+
+/*******************************
+ * Variable mutation and type coercion
+ */
+
+ let firstName = 'John';
+ let age = 28;
+
+ // Type coercion (JavaScript converts numbers/booleans to strings)
+ console.log(firstName + ' ' + age)
+
+ let job, isMarried;
+ job = 'Teacher';
+ isMarried = 'false';
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+ // Variable mutation
+ age = 'twenty eight';
+ job = 'driver';
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+let lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
