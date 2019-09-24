e396a7d6e88e074657afb6a82f7484c51e1743c5
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-24 16:33:53 -0400

    hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,8 +1,28 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+calculateAge(1987);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+}
 
+// retirement(1987);
 
+let retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
+
+///variables
+console.log(age);
+var age = 23;
+console.log(age);
+
+function foo() {
+    let age = 65;
+    console.log(age);
+};
+foo();
+console.log(age);
 
 
 
