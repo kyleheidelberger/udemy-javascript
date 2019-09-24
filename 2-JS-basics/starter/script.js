5931da6873e2937a14033033c486a4b88fe27a04
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-24 13:57:52 -0400

    2.26 objects and methods

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -453,3 +453,24 @@ jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
 */
+
+/*******************************
+ * Objects and Methods
+
+
+let john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(birthYear) {
+        this.age = 2019 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
+ */
+
