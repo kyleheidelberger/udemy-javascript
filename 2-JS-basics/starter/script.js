c0183e0945c7c04ea6473fc7dac5b1e2c0f0ddaa
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-24 09:26:31 -0400

    2.15 boolean logic

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -166,3 +166,24 @@ else {
     console.log('John is fatter than Mark.');
 }
 */
+
+/********************************
+ * Boolean logic
+
+
+let firstName = 'John';
+let age = 20;
+
+if (age < 13) {
+    console.log(firsName + ' is a boy.');
+}
+else if (age >= 13 && age < 20) { // between 13 and 20
+    console.log(firstName + ' is a teenager.');
+}
+else if (age >= 20 && age < 30) { // between 13 and 20
+    console.log(firstName + ' is a young man.');
+}
+else {
+    console.log(firstName + ' is a man.');
+}
+ */
