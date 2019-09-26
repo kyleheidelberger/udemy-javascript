04fe3ddc86acff963892c5bb74981e5b4bfdd7ef
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-26 12:21:07 -0400

    add challenges to pig game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -7,8 +7,13 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+CHALLENGES:
+- Player loses entire score when rolls two 6s in a row (removed)
+- Add input field where players can set winning score
+- Add second die to game. Player loses round score if one is a 1
+
 */
-let scores, roundScore, activePlayer, gamePlaying;
+let scores, roundScore, activePlayer, gamePlaying, lastDice;
 
 newGame();
 
@@ -16,23 +21,41 @@ newGame();
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
     // get random number
-    let dice = Math.floor(Math.random() * 6) + 1;
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+
     // display result
-    let diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
     
     // update round store if rolled num NOT 1
-    if (dice !== 1) {
+    /*if (dice === 6 && lastDice === 6) {
+        // Player loses score
+        scores[activePlayer] = 0;
+        document.querySelector('#score-' + activePlayer).textContent = 0;
+        nextPlayer();
+    } else if (dice !== 1) {
         // Add score
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    }
-    else {
+    } else {
         // End turn; Next player's turn
         nextPlayer();
         }
+    
+    lastDice = dice;
+    */
+    if (dice1 !==1 && dice2 !== 1) {
+        // Add score
+        roundScore += dice1 + dice2;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // End turn; Next player's turn
+        nextPlayer();
     }
+}
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
@@ -43,17 +66,26 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // update interface
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    let input = document.querySelector('.final-score').value;
+    let winningScore;
+    
+    if (input) {
+        winningScore = input;
+    } else {
+        winningScore = 100;
+    }
+
     // check if player won
-    if (scores[activePlayer] >= 5) {
+    if (scores[activePlayer] >= winningScore) {
         document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         gamePlaying = false;
-        } 
-    else {
+    } else {
         nextPlayer();
-    }
+        }
     }
 });
 
@@ -67,7 +99,8 @@ function nextPlayer () {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', newGame);
@@ -85,7 +118,8 @@ function newGame() {
     document.getElementById('current-1').textContent = 0;
 
     // hide die at start of game
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
 
     // reset player names
     document.getElementById('name-0').textContent = 'Player 1';
